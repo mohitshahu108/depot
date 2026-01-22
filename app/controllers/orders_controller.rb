3696930d7698e8ddaa0a956bf8a13993554db330
mohitshahu108 <mohitshahu12126@gmail.com>
2026-01-23 05:27:20 +0530

    Sending Confirmation Emails

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,7 +32,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
       else
@@ -81,4 +81,18 @@ class OrdersController < ApplicationController
         redirect_to store_index_url, notice: "Your cart is empty"
       end
     end
+
+   def pay_type_params
+     case order_params[:pay_type]
+     when "Credit card"
+       params.require(:order).permit(:credit_card_number, :expiration_date)
+     when "Check"
+       params.require(:order).permit(:routing_number, :account_number)
+     when "Purchase order"
+       params.require(:order).permit(:po_number)
+     else
+       {}
+     end
+   end
+
 end
